3f14bcaea6dcb0d764b9a2d0317f74f2621bcb39
Hola <2721495886@qq.com>
2022-09-06 12:10:25 +0800

    修复运行问题

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import App from './App'
 
 // Api函数polyfill（目前为实验版本，如不需要，可删除！）';
-import Polyfill from './polyfill/polyfill';
-Polyfill.init();
+// import Polyfill from './polyfill/polyfill';
+// Polyfill.init();
 
-// 全局mixins，用于实现setData等功能，请勿删除！';
-import Mixin from './polyfill/mixins';
+// // 全局mixins，用于实现setData等功能，请勿删除！';
+// import Mixin from './polyfill/mixins';
 
 // #ifndef VUE3
 import Vue from 'vue'
